0fa1eea34fbf38d213003d9cb79e09d0594e8b22
Gabriel FOURNIER <gfournie@e1r8p1.42.fr>
2016-03-17 19:45:42 +0100

    fds

---
+++ zrtv1/misc.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   misc.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfournie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/15 20:33:02 by gfournie          #+#    #+#             */
+/*   Updated: 2016/03/15 20:33:03 by gfournie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rtv1.h"
+
+int		itemadator(t_env *env, t_item *item)
+{
+	t_item		*copy;
+	int			i;
+
+	i = 1;
+	if (!env->item)
+		env->item = item;
+	else
+	{
+		copy = env->item;
+		while (copy->next)
+		{
+			i++;
+			copy = copy->next;
+		}
+		copy->next = item;
+	}
+	return (i);
+}
