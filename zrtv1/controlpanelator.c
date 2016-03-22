6d0c77eb4edaeb172d02badc58ae8a7fc4d820cc
Gabriel FOURNIER <gfournie@e1r6p2.42.fr>
2016-03-22 19:41:18 +0100

    fds

---
+++ zrtv1/controlpanelator.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   controlpanelator.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfournie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/17 23:24:37 by gfournie          #+#    #+#             */
+/*   Updated: 2016/03/17 23:24:46 by gfournie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rtv1.h"
+
+// t_limg		*new_t_limg2()
+// {
+// 	t_limg	*ret;
+
+// 	ret = malloc(sizeof(t_limg));
+// 	ret->image = NULL;
+// 	ret->img = NULL;
+// 	ret->prev = NULL;
+// 	ret->next = NULL;
+// 	return (ret);
+// }
+
+inline void	*imgcptor(t_env *env)
+{
+	static t_limg	*limg = NULL;
+
+	(void)env;(void)limg;
+	// if (limg == NULL)
+	// {
+	// 	limg = malloc(sizeof(t_limg));
+	// 	limg->image = mlx_new_image(env->mlx, L_SIZEC, H_SIZE);;
+	// 	limg->img = mlx_get_data_addr(limg->image, &limg->bpp, &limg->sline, &env->endiant);
+	// 	limg->prev = NULL;
+	// 	limg->next = NULL;
+	// 	printmusicator(limg, env);
+	// }
+// 	if (env->ft % 2 == 1)
+// 		mlx_put_image_to_window(env->mlx, env->win, limg->image, L_SIZE, 0);
+	return (NULL);
+}
