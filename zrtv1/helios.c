1c8b36468716e41dda4e095327241afad38f8963
Gabriel FOURNIER <gfournie@e2r9p5.42.fr>
2016-06-09 00:01:41 +0200

    fdsf

---
+++ zrtv1/helios.c
@@ -0,0 +1,499 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   helios.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jromagna <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/11 16:59:53 by jromagna          #+#    #+#             */
+/*   Updated: 2016/04/11 16:59:54 by jromagna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rtv1.h"
+//photonic_yolo_director
+
+
+void		print_tout_tree(t_proto *tmp)
+{
+	static int x = -1;
+	static int prof = 0;
+
+	x++;
+	printf("prof %d proto %d = %f] [y = %f] [z = %f]\n", prof, x, tmp->x , tmp->y, tmp->z);
+	printf("color = %d\n", get_color(tmp->color.r, tmp->color.g, tmp->color.b));
+	if (tmp->gauche)
+	{
+		prof++;
+		print_tout_tree(tmp->gauche);
+	}
+	if (tmp->droite)
+	{
+		prof++;
+		print_tout_tree(tmp->droite);
+	}
+	prof--;
+}
+
+void		print_proto(t_proto *tmp)
+{
+		ft_putstr("maillon protolis");
+		printf("proto = x = %f] [y = %f] [z = %f]\n", tmp->x , tmp->y, tmp->z);
+}
+
+void		print_proto_list(t_proto *protolis)
+{
+	t_proto	*tmp;
+	int		i;
+
+	i = 0;
+	tmp = protolis;
+	ft_putstr("\n");
+	while (tmp != NULL)
+	{
+		i++;
+		ft_putstr("maillon protolis = ");
+		printf("%d = [x = %f] [y = %f] [z = %f]\n", i, tmp->x , tmp->y, tmp->z);
+		tmp = tmp->droite;
+	}
+}
+
+int			compx(t_proto *proto1, t_proto *proto2)
+{
+	if (!proto1 || !proto2)
+		return (0);
+	if (proto1->x > proto2->x )
+		return (1);
+	return (0);
+}
+
+int			compy(t_proto *proto1, t_proto *proto2)
+{
+	if (!proto1 || !proto2)
+		return (2);
+	if (proto1->y > proto2->y)
+		return (1);
+	return (0);
+}
+
+int			compz(t_proto *proto1, t_proto *proto2)
+{
+	if (!proto1 || !proto2)
+		return (2);
+	if (proto1->z > proto2->z)
+		return (1);
+	return (0);
+}
+
+int			proto_counter(t_proto *protolis)
+{
+	int		i;
+	t_proto	*tmp;
+
+	i = 0;
+	tmp = protolis;
+	while (tmp != NULL)
+	{
+		tmp = tmp->droite;
+		i++;
+	}
+	return (i);
+}
+
+void	proto_fill(t_inter *inter, t_proto *proto, t_color color)
+{
+
+	(void)color;
+	//ft_putendl("filluuuuuu");
+	proto->x = inter->pos.x;
+	proto->y = inter->pos.y;
+	proto->z = inter->pos.z;
+	proto->color.r = color.r;
+	proto->color.g = color.g; 
+	proto->color.b = color.b;
+	proto->droite = NULL;
+	proto->gauche = NULL;
+}
+
+t_proto		*new_t_proto()
+{
+	t_proto	*proto;
+
+//	ft_putendl("\nNEW PROTO");
+	proto = malloc(sizeof(t_proto));
+	proto->x = 0;
+	proto->y = 0;
+	proto->z = 0;
+	proto->color = new_t_color(0, 0, 0);
+	proto->droite = NULL;
+	proto->gauche = NULL;
+	return (proto);
+}
+
+t_vec		conseiller_d_orientation_protonique_alcolique()
+{
+	return (new_t_vec(((((float)rand()/(float)(RAND_MAX)) * 2) - 1),
+	   ((((float)rand()/(float)(RAND_MAX)) * 2) - 1),
+	  ((((float)rand()/(float)(RAND_MAX)) * 2) - 1)));
+}
+
+t_proto		*range_proton(t_proto *protolis, t_proto *proto, int (*comp)(t_proto *, t_proto *))
+{
+	t_proto *tmp;
+	t_proto	*pre;
+	static int x = 0;
+	static int y = 0;
+	static int z = 0;
+
+	x++;
+	//printf("proton nrb = %d\n", x);
+	//ft_putendl("RANGE LE PROTN PLZ");
+	pre = NULL;
+	tmp = NULL;
+	if (proto == NULL)
+	{
+		y++;
+		//printf("proto null = %d\n", y);
+	//	ft_putendl("LOOOOOOOOOOOOOL");
+		return (protolis);
+	}
+	if (protolis == NULL)
+	{
+		z++;
+	//	printf("protolis NULL = %d\n", z);
+	//	ft_putendl("LAAAAAAAAAA");
+		protolis = proto;
+		return (protolis);
+	}
+
+	tmp = protolis;
+	//ft_putendl("AVANT");
+	while (comp(proto, tmp) == 1)
+	{
+	//	ft_putendl("BOUCLE COMP");
+		pre = tmp;
+		tmp = tmp->droite;
+	}
+	//ft_putendl("apres la boucle");
+	if (pre != NULL)
+	{
+		pre->droite = proto;
+		proto->droite = tmp;
+		return (protolis);
+	}
+	else
+	{
+		proto->droite = tmp;
+		return (proto);
+	}
+	//ft_putendl("toutalafin");
+}
+
+t_proto		*protocopieur(t_proto *protolis)
+{
+	t_proto	*proto;
+
+	proto = malloc(sizeof(t_proto));
+	proto->x = protolis->x;
+	proto->y = protolis->y;
+	proto->z = protolis->z;
+	proto->color = protolis->color;
+	proto->droite = NULL;
+	proto->gauche = NULL;
+	return (proto);
+}
+
+t_proto		*photoinsertor(t_proto *list, int (*comptr)(t_proto *, t_proto *))
+{
+	t_proto	*ret;
+	t_proto	*tmp;
+
+	tmp = list;
+	ret = NULL;
+	//ft_putendl("\ndebut sertor");
+//	print_proto_list(list);
+//	ft_putendl("\n milieu");
+	while (tmp != NULL)
+	{
+		ret = range_proton(ret, protocopieur(tmp), comptr);
+	//	ft_putendl("boucle");
+	//	print_proto_list(ret);
+		tmp = tmp->droite;
+	}
+	// ft_putendl("\nle GRAND FINALEEE");
+//	 print_proto_list(ret);
+	// ft_putendl("finsertor");
+	return (ret);
+}
+
+t_proto		*photon_branch(t_proto *protolis, int prof, int (**comptr)(t_proto *, t_proto *))
+{
+	t_proto	*start;
+	t_proto	*tmp;
+	t_proto	*pre;
+	t_proto *tree;
+	int		profmod;
+	int		count;
+
+	//ft_putendl("\nBRAMCH BRANCH");
+	
+	start = NULL;
+	tmp = NULL;
+	pre = NULL;
+	
+
+	profmod = prof % 3;
+	//printf("prof mod = %d\n", profmod);
+
+	tmp = photoinsertor(protolis, comptr[profmod]);
+
+	count = proto_counter(tmp) / 2;	
+//	printf("count = %d\n", count);
+	
+	start = tmp;
+	while (count > 0)
+	{
+		//ft_putendl("count boucle");
+		pre = tmp;
+		tmp = tmp->droite;
+		count--;
+	}
+	// ft_putendl("laaa1");
+	tree = protocopieur(tmp);
+	// ft_putendl("laaa2");
+	pre->droite = NULL;
+	// ft_putendl("laaa3");
+	tmp = tmp->droite;
+	// ft_putendl("laaa4");
+	if (start && start->droite != NULL)
+	{
+		// ft_putendl("laaa5");
+		tree->gauche = photon_branch(start, profmod + 1, comptr);
+		// ft_putendl("laaa6");
+	}
+	else
+	{
+		// ft_putendl("laaa7");
+		tree->gauche = start;
+		// ft_putendl("laaa8");
+	}
+	if (tmp && tmp->droite != NULL)
+	{
+		// ft_putendl("laaa9");
+		tree->droite = photon_branch(tmp, profmod + 1, comptr);
+		// ft_putendl("laaa10");
+	}
+	else
+	{
+		// ft_putendl("laaa11");
+		tree->droite = tmp;
+		// ft_putendl("laaa12");
+	}
+	// ft_putendl("return tree");
+	return (tree);
+}
+
+// t_proto		*photon_branch(t_proto *protolis, int prof, int i, int (**comptr)(t_proto *, t_proto *))
+// {
+// 	t_proto	*start;
+// 	t_proto	*tmp;
+// 	t_proto	*pre;
+// 	t_proto *tree;
+// 	int		profmod;
+// 	int		count;
+
+// 	ft_putendl("\nBRAMCH BRANCH");
+// 	printf("count = %d\n", i);
+// 	start = NULL;
+// 	tmp = NULL;
+// 	pre = NULL;
+// 	count = i / 2;
+// 	profmod = prof % 3;
+// 	printf("prof mod = %d\n", profmod);
+
+// 	tmp = photoinsertor(protolis, comptr[profmod]);
+
+// 	start = tmp;
+// 	while (count > 0)
+// 	{
+// 		pre = tmp;
+// 		tmp = tmp->droite;
+// 		count--;
+// 	}
+// 	tree = protocopieur(tmp);
+// 	pre->droite = NULL;
+// 	tmp = tmp->droite;
+// 	if (start->gauche != NULL)
+// 		tree->gauche = photon_branch(start, profmod + 1, i / 2 , comptr);
+// 	else
+// 		tree->gauche = start;
+// 	if (tmp->droite != NULL)
+// 		tree->droite = photon_branch(tmp, profmod + 1, i / 2 , comptr);
+// 	else
+// 		tree->droite = tmp;
+// 	ft_putendl("return tree");
+// 	return (tree);
+// }
+
+t_proto		*photosynthetisator_spatial(t_proto *protolis)
+{
+	t_proto	*prototree;
+	int 	(*comptr[3])(t_proto *, t_proto *);
+	
+	prototree = NULL;
+
+	comptr[0] = compx;
+	comptr[1] = compy;
+	comptr[2] = compz;
+	prototree = photon_branch(protolis, 0, comptr);
+	return (prototree);
+}
+
+t_vec	ref_ret(t_inter *inter, t_pd *pd)
+{
+	return (normalizator_ret(miroiratorvcalculator(pd->dir, inter->norm)));
+}
+
+t_vec 	trans_calculator_ret(t_inter *inter, t_pd *pd)
+{
+	FLOAT_SIZE	scalc;
+	FLOAT_SIZE	angle;
+	t_vec		t;
+
+	t.x = 0;
+	t.y = 0;
+	t.z = 0;
+	angle = 0;
+	scalc = 0;
+
+	angle = M_PI_2 - acos(dot_prod(pd->dir, inter->norm));
+	angle = (angle > 0) ? -angle : angle;
+	// if (sin(angle) > (AIR_INCI / GLASS_INCI))
+	// 	return (NULL);
+	scalc = carre(AIR_INCI / GLASS_INCI) * carre(1 - carre(cos(angle)));
+	t.x = ((AIR_INCI / GLASS_INCI) * pd->dir.x) + ((AIR_INCI / GLASS_INCI) * cos(angle) - fabs(1 - scalc)) *  inter->norm.x;
+	t.y = ((AIR_INCI / GLASS_INCI) * pd->dir.y) + ((AIR_INCI / GLASS_INCI) * cos(angle) - fabs(1 - scalc)) *  inter->norm.y;
+	t.z = ((AIR_INCI / GLASS_INCI) * pd->dir.z) + ((AIR_INCI / GLASS_INCI) * cos(angle) - fabs(1 - scalc)) *  inter->norm.z;
+	return (normalizator_ret(t));
+}
+
+
+float		get_color_str(t_color *color)
+{
+	float	sub;
+
+	sub = color->r + color->g + color->b;
+	return (sub / 768 * 100);
+}
+
+
+
+t_proto			*lance_soleil(t_pd *pd, t_proto	*protolis, t_item *item, t_color licolor)
+{
+	static int		test = 0;
+	t_inter			inter;
+	float			bang;
+	int				i;
+	float			diffpow;
+	t_proto			*proto;
+
+	diffpow = 0;
+	
+	//printf("test = %d\n", test);
+	i = 0;
+	
+	proto = NULL;
+	bang = ((float)rand()/(float)(RAND_MAX));
+	while (i <= 8)
+	{
+		test++;
+		t_inter_set(&inter);
+		impactor(pd, item , &inter);
+		set_inter_pos(&inter, pd);
+		bang = ((float)rand()/(float)(RAND_MAX));
+	//	printf("bang = %f\n", bang);
+		diffpow = get_color_str(&inter.diff);
+		if (bang <= diffpow)
+		{
+			if (inter.trans == 0)
+			{
+				proto = new_t_proto();
+				proto_fill(&inter, proto, licolor);
+				protolis = range_proton(protolis, proto, &compx);
+				// if (protolis == NULL)
+				// 	ft_putendl("toujours NULL");
+				// else
+				// 	ft_putendl("sortie bien");
+				pd->pos = inter.pos;
+				pd->dir = normalizator_ret(conseiller_d_orientation_protonique_alcolique());
+			}
+			else if (inter.trans == 1)
+			{
+				pd->dir = trans_calculator_ret(&inter, pd);
+				pd->pos = inter.pos;
+			}
+			continue ;
+		}
+		else if (bang > diffpow && bang < inter.ref + diffpow)
+		{
+			pd->dir = ref_ret(&inter, pd);
+			pd->pos = inter.pos;
+			continue ;
+		}
+		else if (inter.ref == 0 && inter.trans == 0)
+		{
+			proto = new_t_proto();
+			proto_fill(&inter, proto, licolor);
+			protolis = range_proton(protolis, proto, &compx);
+			// if (protolis == NULL)
+			// 	ft_putendl("toujours NULL");
+			// else
+			// 	ft_putendl("sortie bien");
+			return (protolis);
+		}
+		else 
+			return (protolis);
+		i++;
+	}
+	return (protolis);
+}
+
+t_proto		*helios(t_item *item, t_light *light, t_proto *prototree)
+{
+	int		photon;
+	t_pd	pd;
+	t_proto	*protolis;
+
+	ft_putendl("PRAISE THE SUUUUUUUN !!");
+	protolis = NULL;
+	srand(time(NULL));
+	while (light != NULL)
+	{
+		ft_putendl("BOUCLE");
+		photon = 0;
+		while (photon < light->photon)
+		{
+			//ft_putendl("BOUCLE2");
+			pd.pos = light->pos;
+			pd.dir = normalizator_ret(conseiller_d_orientation_protonique_alcolique());
+			protolis = lance_soleil(&pd, protolis, item, light->rcolor);
+			photon++;
+		}
+		ft_putendl("BOUCLE fin");
+		light = light->next;
+	}
+	
+	//print_proto_list(protolis);
+
+	if (protolis == NULL)
+		ft_putendl("NUUUUUUULLLLLLLLLL");
+	ft_putendl("GROW PLZ TREE");
+	prototree = photosynthetisator_spatial(protolis);
+	ft_putendl("HELIOS IS FINISHED");
+//	print_tout_tree(prototree);
+	return (prototree);
+}
+
+
+
+
